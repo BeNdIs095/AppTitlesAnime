0dc1188a767a3ec4f24cc0b10cb63f3e4c115613
Жариков Антон <gasteranton@gmail.com>
2025-04-10 16:01:41 +0500

    Добавлен вывод типов аниме

--- AppTitlesAnime/FormListTypes.cs
+++ AppTitlesAnime/FormListTypes.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppTitlesAnime.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AppContext = AppTitlesAnime.Models.AppContext;
 
 namespace AppTitlesAnime
 {
     public partial class FormListTypes : Form
     {
+        private AppContext db;
+
         public FormListTypes()
         {
             InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            this.db = new AppContext();
+            this.db.Types.Load();
+            this.dataGridViewTypes.DataSource = this.db.Types.Local.OrderBy(o => o.TypeName).ToList();
         }
 
         private void FlowLayoutPanel1_Paint(object sender, PaintEventArgs e)
