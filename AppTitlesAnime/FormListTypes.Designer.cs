f6b3ff33f572bd0b6bc9b6008eca8117be2c1354
Жариков Антон <gasteranton@gmail.com>
2025-04-10 15:40:55 +0500

    Добавлены формы FormAddType, FormListTypes, FormMain

---
+++ AppTitlesAnime/FormListTypes.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace AppTitlesAnime
+{
+    partial class FormListTypes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flowLayoutPanelTop = new FlowLayoutPanel();
+            btnAddType = new Button();
+            btnUpdateType = new Button();
+            btnDeleteType = new Button();
+            panelFill = new Panel();
+            dataGridViewTypes = new DataGridView();
+            flowLayoutPanelTop.SuspendLayout();
+            panelFill.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewTypes).BeginInit();
+            SuspendLayout();
+            // 
+            // flowLayoutPanelTop
+            // 
+            flowLayoutPanelTop.AutoSize = true;
+            flowLayoutPanelTop.BackColor = Color.White;
+            flowLayoutPanelTop.Controls.Add(btnAddType);
+            flowLayoutPanelTop.Controls.Add(btnUpdateType);
+            flowLayoutPanelTop.Controls.Add(btnDeleteType);
+            flowLayoutPanelTop.Dock = DockStyle.Top;
+            flowLayoutPanelTop.Location = new Point(0, 0);
+            flowLayoutPanelTop.Name = "flowLayoutPanelTop";
+            flowLayoutPanelTop.Padding = new Padding(10);
+            flowLayoutPanelTop.Size = new Size(850, 57);
+            flowLayoutPanelTop.TabIndex = 0;
+            flowLayoutPanelTop.Paint += FlowLayoutPanel1_Paint;
+            // 
+            // btnAddType
+            // 
+            btnAddType.Location = new Point(13, 13);
+            btnAddType.Name = "btnAddType";
+            btnAddType.Size = new Size(111, 31);
+            btnAddType.TabIndex = 0;
+            btnAddType.Text = "Добавить";
+            btnAddType.UseVisualStyleBackColor = true;
+            btnAddType.Click += BtnAddType_Click;
+            // 
+            // btnUpdateType
+            // 
+            btnUpdateType.Location = new Point(130, 13);
+            btnUpdateType.Name = "btnUpdateType";
+            btnUpdateType.Size = new Size(111, 31);
+            btnUpdateType.TabIndex = 1;
+            btnUpdateType.Text = "Обновить";
+            btnUpdateType.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteType
+            // 
+            btnDeleteType.Location = new Point(247, 13);
+            btnDeleteType.Name = "btnDeleteType";
+            btnDeleteType.Size = new Size(91, 31);
+            btnDeleteType.TabIndex = 2;
+            btnDeleteType.Text = "Удалить";
+            btnDeleteType.UseVisualStyleBackColor = true;
+            // 
+            // panelFill
+            // 
+            panelFill.Controls.Add(dataGridViewTypes);
+            panelFill.Dock = DockStyle.Fill;
+            panelFill.Location = new Point(0, 57);
+            panelFill.Name = "panelFill";
+            panelFill.Padding = new Padding(10);
+            panelFill.Size = new Size(850, 440);
+            panelFill.TabIndex = 1;
+            // 
+            // dataGridViewTypes
+            // 
+            dataGridViewTypes.BackgroundColor = Color.White;
+            dataGridViewTypes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewTypes.Dock = DockStyle.Fill;
+            dataGridViewTypes.Location = new Point(10, 10);
+            dataGridViewTypes.Name = "dataGridViewTypes";
+            dataGridViewTypes.ReadOnly = true;
+            dataGridViewTypes.Size = new Size(830, 420);
+            dataGridViewTypes.TabIndex = 0;
+            // 
+            // FormListTypes
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(850, 497);
+            Controls.Add(panelFill);
+            Controls.Add(flowLayoutPanelTop);
+            Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Margin = new Padding(5);
+            Name = "FormListTypes";
+            Text = "Список типов аниме";
+            flowLayoutPanelTop.ResumeLayout(false);
+            panelFill.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridViewTypes).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanelTop;
+        private Button btnAddType;
+        private Button btnUpdateType;
+        private Button btnDeleteType;
+        private Panel panelFill;
+        private DataGridView dataGridViewTypes;
+    }
+}
